78fa543e7c8e45f91f7b7fddbe8a64664ac271c2
Bindiamohindru <bindiamohindru@hotmail.co.uk>
2020-10-05 09:45:55 +0000

    Too high

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a guess the number game.
+import random
+ 
+guessesTaken = 0
+
+print('Hello! What is your name?')
+myName = input()
+ 
+number = random.randint(1, 10)  
+print('Well, ' + myName + ', I am thinking of a number between 1 and 10.')
+
+while guessesTaken < 6:
+       print('Take a guess.') 
+       guess = input()
+       guess = int(guess)
+
+       guessesTaken = guessesTaken + 1
+
+       if guess > number:
+             print('Your guess is too low.') 
+
+       if guess < number:
+             print('Your guess is too high.')
